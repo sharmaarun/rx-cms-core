9bf500f91dcdea34cb76abbf5dd7aa8e7df5d8d9
Arun Sharma <sharma.arun333@gmail.com>
2021-05-10 02:51:07 +0530

    modified api method to be an array of strings

--- src/interfaces/api/index.ts
+++ src/interfaces/api/index.ts
@@ -35,7 +35,7 @@ export interface IAPI {
  */
 export interface IEndpointConfig {
   path: string;
-  method: IAPIMethod;
+  method: IAPIMethod | IAPIMethod[];
   controller: IAPIController;
   policies?: string[];
 }
